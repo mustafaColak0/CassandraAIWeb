db9e85a368917da992834b30b9d9270a5430d980
Mustyy <25640140050@mersin.edu.tr>
2026-08-17 15:45:42 +0300

    Update app.js

--- app.js
+++ app.js
@@ -299,7 +299,7 @@ async function runAnalysis() {
 
         let messageContent;
         // Groq üzerindeki en güncel ve kararlı metin/log modeli
-        let selectedModel = "llama-3.3-70b-versatile"; 
+        let selectedModel = "llama-3.1-70b-versatile"; 
 
         if (base64Image) {
             // EĞER RESİM VARSA: Groq'un vizyon modeline geçiş yapıyoruz
